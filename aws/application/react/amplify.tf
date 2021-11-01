f0fc0d144670d5b09c63ed1f0dd3f2c5cf0b88fb
Otherotter <email>
2021-11-01 15:57:25 +0000

    Added files

--- aws/application/react/amplify.tf
+++ aws/application/react/amplify.tf
@@ -1,8 +1,8 @@
 resource "aws_amplify_app" "example" {
-  name       = "wireframe "
+  name       = "todolist"
   repository = "https://github.com/Otherotter/todo_hw2.git"
   # GitHub personal access token
-  access_token =  "~/.ssh/id_rsa.pub"
+  access_token =  "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"
 
   # The default build_spec added by the Amplify Console for React.
   build_spec = <<-EOT
@@ -33,5 +33,17 @@ resource "aws_amplify_app" "example" {
 
   environment_variables = {
     ENV = "test"
+  }
+}
+
+resource "aws_amplify_branch" "master" {
+  app_id      = aws_amplify_app.example.id
+  branch_name = "master"
+
+  framework = "React"
+  stage     = "PRODUCTION"
+
+  environment_variables = {
+    REACT_APP_API_SERVER = "https://api.example.com"
   }
 }
